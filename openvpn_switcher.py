e49aa5e061bafb7fa90f02889397b720a9ec9b4b
jethigh <jethigh@protonmail.com>
2018-09-18 01:03:13 +0200

    Dodanie metody sat_config (do poprawek)

--- openvpn_switcher.py
+++ openvpn_switcher.py
@@ -52,6 +52,7 @@ class ServiceMonitor(object):
         self.stop()
         self.start()
 
+
 class TZ_Server(object):
     def __init__(self, server):
         self.server = server
@@ -60,17 +61,41 @@ class TZ_Server(object):
         '''
         Funkcja zwraca True jezeli podany server jest w konfiguracji.
         W przeciwnym przypadku zwraca False
+        Funkcja read() dla pliku wczytuje cały plik jako jeden string,
+        stąd nie można użyć w re ^ jako początku lini tylko poslużylem
+        sie znakiem nowej lini.
         '''
         with open(CONFIG, 'r') as config:
-            match = re.search(str(self.server), config)
+            #match = re.search('^remote ' + str(self.server), config.read())
+            match = re.search(r'\nremote ' + self.server, config.read())
             if match:
                 return True
             else:
                 return False
+        config.closed
+
+    def set_config(self):
+        '''
+        Metoda ustawia te instancje klasy w konfiguracji OpenVPN jako aktywny
+        serwer
+        '''
+        new_content = ''
+        if self.in_config():
+            print('Serwer ' + self.server + ' jest już w konfiguracji')
+        else:
+            with open(CONFIG, 'r+') as config:
+                for line in config:
+                    if re.search(r'^remote ', line):
+                        line = re.sub('remote .+ ','remote ' + self.server + ' ',line)
+                        new_content += line
+                    else:
+                        new_content += line
+                print(new_content)
+                #config.write(new_content)
 
 
 if __name__ == '__main__':
     serwis = ServiceMonitor('cups')
-    server = TZ_Server('pl.trust.zone')
-    server.in_config()
+    server = TZ_Server('de.trust.zone')
+    server.set_config()
     #serwis.restart()
